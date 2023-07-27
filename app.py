820e6fa99ebc86d954cac9b3fd76024438c57183
MikeBorman1 <mike.borman@yahoo.com>
2023-07-27 17:09:26 +0700

    fixed issues

--- app.py
+++ app.py
@@ -1,42 +1,73 @@
-# Import os to set API key
+
 import os
 # Import OpenAI as main LLM service
 from langchain.llms import OpenAI
 from langchain.embeddings import OpenAIEmbeddings
 # Bring in streamlit for UI/app interface
 import streamlit as st
-
+import pandas as pd
+from transformers import GPT2TokenizerFast
+import matplotlib.pyplot as plt
 # Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
 # Import chroma as the vector store 
 from langchain.vectorstores import Chroma
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 # Import vector store stuff
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
     VectorStoreToolkit,
     VectorStoreInfo
 )
+from transformers import GPT2TokenizerFast
+from langchain.text_splitter import CharacterTextSplitter
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = ''
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Create instance of OpenAI LLM
-llm = OpenAI(temperature=0.1, verbose=True)
+llm = OpenAI(temperature=0.1, verbose=True, max_tokens=3000)
 embeddings = OpenAIEmbeddings()
 
 # Create and load PDF Loader
-loader = PyPDFLoader('Employee-Handbook.pdf')
+#loader = PyPDFLoader('Polar.pdf')
 # Split pages from pdf 
-pages = loader.load_and_split()
+#pages = loader.load_and_split()
 # Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(pages, embeddings, collection_name='handbook')
+
+# Advanced method - Split by chunk
+
+# importing required modules
+from PyPDF2 import PdfReader
+  
+reader = PdfReader('Polar.pdf')
+
+raw_text = ''
+for i, page in enumerate(reader.pages):
+    text = page.extract_text()
+    if text:
+        raw_text += text
+
+
+
+
+text_splitter = CharacterTextSplitter(        
+    separator = "\n",
+    chunk_size = 500,
+    chunk_overlap  = 100,
+    length_function = len,
+)
+texts = text_splitter.split_text(raw_text)
+
+
+
+store = Chroma.from_texts(texts, embeddings, collection_name='polarlounge')
 
 # Create vectorstore info object - metadata repo?
 vectorstore_info = VectorStoreInfo(
-    name="employee handbook",
-    description="employee handbook as a pdf",
+    name="Polar Lounge",
+    description="Polar Lounge Bot",
     vectorstore=store
 )
 # Convert the document store into a langchain toolkit
@@ -46,11 +77,12 @@ toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 agent_executor = create_vectorstore_agent(
     llm=llm,
     toolkit=toolkit,
-    verbose=True
+    verbose=True,
+    reduce_k_below_max_tokens=True
 )
-st.title('🦜🔗 Employee Handbook Agent')
+st.title('Polar Lounge Bot')
 # Create a text input box for the user
-prompt = st.text_input('Input your prompt here')
+prompt = st.text_input('Input your question here')
 
 # If the user hits enter
 if prompt:
